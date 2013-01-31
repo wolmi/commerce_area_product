eef0939006dcfcaf6cee97929a7a1fc7dbf6b457
Miquel Ramon Ortega i Tido <wolmi@386414.no-reply.drupal.org>
2013-01-31 20:02:09 +0100

    Ejemplo de las funciones

---
+++ js/commerce_area_product.js
@@ -0,0 +1,10 @@
+(function($) {
+	
+	Drupal.behaviors.commerceAreaProduct = {
+		attach: function() {
+			/* Aqui hacemos la comprobación de introducción de datos
+			 * y la llamada Ajax para recalcular el precio
+			 */
+		}
+	}
+})
